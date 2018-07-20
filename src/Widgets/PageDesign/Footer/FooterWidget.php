1d4b08213dbfeed9f687afce96c969211e33c860
cjohannsen <christoph.johannsen@plentymarkets.com>
2018-07-20 12:52:43 +0200

    ADD FooterWidget, CopyrightWidget - 2 new widgets to create a footer

---
+++ src/Widgets/PageDesign/Footer/FooterWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\PageDesign\Footer;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class FooterWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.PageDesign.Footer.FooterWidget";
+}
---
+++ src/Widgets/PageDesign/Footer/Partials/FooterCopyrightWidget.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ceres\Widgets\PageDesign\Footer\Partials;
+
+use Ceres\Widgets\Helper\BaseWidget;
+
+class FooterCopyrightWidget extends BaseWidget
+{
+    protected $template = "Ceres::Widgets.PageDesign.Footer.Partials.FooterCopyrightWidget";
+}
